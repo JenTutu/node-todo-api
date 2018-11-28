891938a551011d1b7357fd2b653a1c37f6935366
Jennifer <JennieO23@gmail.com>
2018-11-28 06:34:44 -0600

    Init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,33 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb'); // creates a var called MongoClient and pulls off the MongoClient object off of MongoDb, identical to above
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => { // db is db object
+  if (err) {
+    return console.log('Unable to connect to MongoDB Server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  // db.collection('Users').insertOne({
+  //   name: 'Jennifer',
+  //   age: 28,
+  //   location: 'Malibu, CA'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert user', err);
+  //   }
+  //   console.log(result.ops[0]._id.getTimestamp()); 
+  // });
+
+  db.close();
+})
