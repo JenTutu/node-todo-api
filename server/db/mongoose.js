15eae0d592adaf33e7f8f5086cc5d19eefdc34ae
Jennifer <JennieO23@gmail.com>
2018-12-07 05:17:48 -0600

    Fixing db connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,5 +3,5 @@ var mongoose = require('mongoose');
 //tell mongoose we want to use its Promise library
 mongoose.Promise = global.Promise;
 // needs to connect to db
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 module.exports = {mongoose}
